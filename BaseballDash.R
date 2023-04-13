206f96f7f941413d95f5240c379b52a77fb6d6e9
Tyler Gorecki <98352086+tylergorecki@users.noreply.github.com>
2023-04-13 11:10:00 -0400

    Add files via upload

--- BaseballDash.R
+++ BaseballDash.R
@@ -1,63 +1,71 @@
 library(ggplot2)
 library(tidyverse)
+library(shiny)
+library(DT)
 
 files <- list.files(pattern = "*.csv")
 games <- lapply(files, read.csv, header = TRUE) 
-
 allGames <- bind_rows(games)
 
 
+#pitcher <- allGames[allGames$PitcherTeam=='Virginia 2023',]$Pitcher
 pitcher <- allGames$Pitcher
 
-
-ui = fluidPage(
-  titlePanel("UVA Baseball Dashboard 2023"),
-  sidebarPanel(h6("Select pitcher below: "), 
-               selectInput("pitcher", "Pitcher", choices = pitcher), 
-               actionButton("game", "Generate Pitcher Data")),
-  tabPanel("Page 1",
-           "This is the content for page 1"),
-  tabPanel("Page 2",
-           "This is the content for page 2"),
-  mainPanel(textOutput("text"))
-)
-
-ui <- navbarPage(
-  "UVA Baseball Dashboard 2023",
-  # Add pages
-  tabPanel("Page 1",
-           "This is the content for page 1"),
-  tabPanel("Page 2",
-           "This is the content for page 2")
-)
-
 ui <- fluidPage(
-  titlePanel("UVA Baseball Dashboard 2023"),
+  titlePanel("UVA Baseball Pitching Dashboard"),
+  tags$head(
+    tags$style(
+      HTML(
+        "
+        
+        /* Change the background color of the navbar */
+        .navbar {
+          background-color: #E57200;
+        }
+        
+        /* Change the color of the text in the navbar */
+        .navbar-default .navbar-nav > li > a {
+          color: white;
+        }
+        
+        /* Change the color of the active tab */
+        .navbar-default .navbar-nav > .active > a,
+        .navbar-default .navbar-nav > .active > a:focus,
+        .navbar-default .navbar-nav > .active > a:hover {
+          background-color: #232D4B;
+          color: white;
+        }
+        "
+      )
+    )
+  ),
   navbarPage(
-    "Pages",
+    title = " ",
     # Page 1
-    tabPanel("Page 1",
+    tabPanel(title = "Summaries",
+             mainPanel(h6("Select pitcher below: "), 
+                          selectInput("pitcher", "Pitcher", choices = pitcher, selected = "Brian Edgington")),
              fluidRow(
-               column(6, h2("Welcome to Page 1!"))
+               verbatimTextOutput("myoutput")
              ),
              fluidRow(
                column(12, p("This is the content for Page 1."))
-             ) 
+             ), 
+             splitLayout(
+               plotOutput("plot1"), 
+               plotOutput("plot2")
+             )
     ),
     # Page 2
-    tabPanel("Page 2",
-             fluidRow(
-               column(6, h2("Welcome to Page 2!"))
-             ),
-             fluidRow(
-               column(12, p("This is the content for Page 2."))
+    tabPanel("Full Season",
+             mainPanel(
+               DT::dataTableOutput("mytable")
              )
     ),
     # Page 3
-    tabPanel("Page 3",
+    tabPanel("Other Data",
              fluidRow(
-               column(6, h2("Welcome to Page 3!")),
-               column(6, img(src = "https://placekitten.com/300/300"))
+               column(6, h2("Welcome to Page 3!"))
              ),
              fluidRow(
                column(12, p("This is the content for Page 3."))
@@ -68,17 +76,26 @@ ui <- fluidPage(
 
 server = function(input, output) {
   
-  observeEvent(input$click, {
-    output$text = renderPrint(
-      allGames[,allGames$Pitcher == 'pitcher']
-    )
+  files <- list.files(pattern = "*.csv")
+  games <- lapply(files, read.csv, header = TRUE) 
+  allGames <- bind_rows(games)
+  
+  mydata <- read.csv("/Users/tylergorecki/Desktop/SASL/Borderline Pitches Project/NYY_data.csv")
+  
+  output$mytable <- DT::renderDataTable({
+    DT::datatable(allGames[allGames$PitcherTeam=='Virginia 2023', ])
   })
   
-  observeEvent(input$game, {
-    output$test = renderPrint(
-      #allGames[,allGames$Pitcher == input$pitcher]
-      input$pitcher
-    )
+  output$myoutput <- renderPrint({
+    input$pitcher
+  })
+  
+  output$plot1 <- renderPlot({
+    plot(1,2)
+  })
+  
+  output$plot2 <- renderPlot({
+    plot(2,1)
   })
 }
 
